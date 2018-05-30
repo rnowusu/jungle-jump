fb85f5ff2103334210c2715a3eadea7809968ccf
Ryan <rnowusu@gwu.edu>
2018-05-29 21:07:24 -0400

    fixed event listeners and util files to make keyboard movement possible

--- bundle.js
+++ bundle.js
@@ -125,13 +125,10 @@ document.addEventListener('DOMContentLoaded', function () {
   // let srcX = currentFrame * width + 6.2;
   // let srcY = 0 * height
 
-  // ctx.drawImage(sprite, srcX, srcY, width, height, x, y, width, height)
   var player = new _moving_object2.default(sprite, srcX, srcY, width, height, x, y, width, height);
   window.player = player;
-  // ctx.drawImage(sprite, x, y, 41, 41)
   //   ctx.fillStyle = 'blue';
   // ctx.fillRect(10, 10, 100, 100);
-  // ctx.drawImage(player.img, player.srcX, player.srcY, player.width, player.height, player.x, player.y, player.width, player.height)
   var a = 0;
   var updateFrame = function updateFrame() {
 
@@ -180,7 +177,7 @@ document.addEventListener('DOMContentLoaded', function () {
     return (0, _utils.mouseupDown)(e, player);
   });
   document.addEventListener('keydown', function (e) {
-    (0, _utils.handleKeyDown)(e, player = player)();
+    (0, _utils.handleKeyDown)(e, player);
   });
   document.addEventListener('keyup', function (e) {
     (0, _utils.handleKeyUp)(e.keyCode)();
@@ -196,156 +193,6 @@ document.addEventListener('DOMContentLoaded', function () {
   requestAnimationFrame(drawImage);
   window.drawImage = drawImage;
 
-  // let sprite = new Image();
-  // sprite.src = './assets/icy_tower_sprites2.png'
-  // sprite.className = "sprite"
-  //
-  // let x = 350;
-  // let y = 380;
-  //
-  // let srcX;
-  // let srcY;
-  //
-  // let sheetWidth = 656;
-  // let sheetHeight = 278;
-  //
-  // let cols = 18;
-  // let rows = 4
-  //
-  // let width = sheetWidth/ cols;
-  // let height = sheetHeight / rows;
-  //
-  // let currentFrame = 0;
-  //
-  // let mouseDownID = -1;
-  //
-  // let handleKeyDown = (n) => {
-  //   switch(n){
-  //     case 37:
-  //     return mousedownLeft;
-  //
-  //     case 38:
-  //     return mousedownUp;
-  //
-  //     case 39:
-  //     return mousedownRight;
-  //
-  //     case 40:
-  //     return mousedownDown;
-  //
-  //     default:
-  //     return () =>{};
-  //   }
-  // };
-  //
-  //
-  // const controls = {
-  //   left: false,
-  //   right: false,
-  //   up: false,
-  //   down: false
-  // }
-  //
-  // let handleKeyUp = (n) => {
-  //   switch(n){
-  //     case 37:
-  //     return mouseupLeft;
-  //
-  //     case 38:
-  //     return mouseupUp;
-  //
-  //     case 39:
-  //     return mouseupRight;
-  //
-  //     case 40:
-  //     return mouseupDown;
-  //
-  //     default:
-  //     return () => {};
-  //   }
-  // };
-  //
-  // let mousedownRight = (e) => {
-  //   if (mouseDownID ==-1) {
-  //     mouseDownID = setInterval(moveRight, 20)
-  //   }
-  // }
-  // let mouseupRight = (e) => {
-  //   if (mouseDownID != -1) {
-  //     clearInterval(mouseDownID)
-  //     mouseDownID = -1;
-  //   }
-  // }
-  //
-  // let mousedownLeft = (e) => {
-  //   if (mouseDownID ==-1) {
-  //     mouseDownID = setInterval(moveLeft, 20)
-  //   }
-  // }
-  // let mouseupLeft = (e) => {
-  //   if (mouseDownID != -1) {
-  //     clearInterval(mouseDownID)
-  //     mouseDownID = -1;
-  //   }
-  // }
-  //
-  // let mousedownUp = (e) => {
-  //   if (mouseDownID ==-1) {
-  //     mouseDownID = setInterval(moveUp, 20)
-  //   }
-  // }
-  // let mouseupUp = (e) => {
-  //   if (mouseDownID != -1) {
-  //     clearInterval(mouseDownID)
-  //     mouseDownID = -1;
-  //   }
-  // }
-  //
-  // let mousedownDown = (e) => {
-  //   if (mouseDownID ==-1) {
-  //     mouseDownID = setInterval(moveDown, 20)
-  //   }
-  // }
-  // let mouseupDown = (e) => {
-  //   if (mouseDownID != -1) {
-  //     clearInterval(mouseDownID)
-  //     mouseDownID = -1;
-  //   }
-  // }
-  //
-  // let moveLeft = () => {
-  //   ctx.clearRect(x, y, width, height)
-  //   x -= 10;
-  //   // srcY -=50;
-  // }
-  //
-  // let moveRight = () => {
-  //   ctx.clearRect(x, y, width, height)
-  //   x += 10;
-  //   // srcX +=50;
-  // }
-  //
-  // let moveUp = () => {
-  //   ctx.clearRect(x, y, width, height)
-  //   y -= 10;
-  // }
-  //
-  // let moveDown = () => {
-  //   ctx.clearRect(x, y, width, height)
-  //   y += 10;
-  // }
-  // document.getElementById('button-right').addEventListener("mousedown", mousedownRight)
-  // document.getElementById('button-right').addEventListener("mouseup", mouseupRight)
-  // document.getElementById('button-left').addEventListener("mousedown", mousedownLeft)
-  // document.getElementById('button-left').addEventListener("mouseup", mouseupLeft)
-  // document.getElementById('button-up').addEventListener("mousedown", mousedownUp)
-  // document.getElementById('button-up').addEventListener("mouseup", mouseupUp)
-  // document.getElementById('button-down').addEventListener("mousedown", mousedownDown)
-  // document.getElementById('button-down').addEventListener("mouseup", mouseupDown)
-  //
-  // document.addEventListener('keydown', (e) => {handleKeyDown(e.keyCode)()})
-  // document.addEventListener('keyup', (e) => {handleKeyUp(e.keyCode)()})
-  //
   // let a = 1;
   // let updateFrame = () => {
   //   // if (currentFrame === 0){
@@ -364,33 +211,8 @@ document.addEventListener('DOMContentLoaded', function () {
   //   }
   //   srcX = currentFrame * width + 6.2;
   //   srcY = 0 * height
-  //
-  //   if (x >= 670 ){
-  //     x = 670;
-  //   } else if (x < 0){ x = 0 }
-  //   if (y <= -40 ){
-  //     y = -40;
-  //   } else if (y > 370) {
-  //     y = 370;
-  //   }
   // }
-  //
-  // let drawImage = () => {
-  //   updateFrame();
-  //   ctx.drawImage(sprite, srcX, srcY, width, height, x, y, width, height)
-  //   requestAnimationFrame(drawImage)
-  // }
-  //
-  // requestAnimationFrame(drawImage);
-  // window.drawImage = drawImage
-  // document.body.childNodes[1].appendChild(sprite)
 });
-
-// export default player;
-// MovingObject.prototype.draw = (ctx) => {
-//   ctx.fillStyle = this.sprite
-// };
-// window.MovingObject = MovingObject;
 
 /***/ }),
 
@@ -452,18 +274,19 @@ Object.defineProperty(exports, "__esModule", {
 var mouseDownID = exports.mouseDownID = -1;
 
 var handleKeyDown = exports.handleKeyDown = function handleKeyDown(e, player) {
+  debugger;
   switch (e.keyCode) {
     case 37:
-      return mousedownLeft(player);
+      return mousedownLeft(e, player);
 
     case 38:
-      return mousedownUp(player);
+      return mousedownUp(e, player);
 
     case 39:
-      return mousedownRight(player);
+      return mousedownRight(e, player);
 
     case 40:
-      return mousedownDown(player);
+      return mousedownDown(e, player);
 
     default:
       return function () {};
@@ -497,6 +320,7 @@ var handleKeyUp = exports.handleKeyUp = function handleKeyUp(n) {
 };
 
 var mousedownRight = exports.mousedownRight = function mousedownRight(e, player) {
+  debugger;
   if (mouseDownID == -1) {
     exports.mouseDownID = mouseDownID = setInterval(function () {
       return moveRight(player);
--- jungle_jump.js
+++ jungle_jump.js
@@ -38,13 +38,10 @@ document.addEventListener('DOMContentLoaded', () =>{
   // let srcX = currentFrame * width + 6.2;
   // let srcY = 0 * height
 
-  // ctx.drawImage(sprite, srcX, srcY, width, height, x, y, width, height)
   let player = new MovingObject(sprite, srcX, srcY, width, height, x, y, width, height)
   window.player = player;
-  // ctx.drawImage(sprite, x, y, 41, 41)
 //   ctx.fillStyle = 'blue';
 // ctx.fillRect(10, 10, 100, 100);
-  // ctx.drawImage(player.img, player.srcX, player.srcY, player.width, player.height, player.x, player.y, player.width, player.height)
   let a = 0;
   let updateFrame = () => {
 
@@ -74,7 +71,7 @@ document.addEventListener('DOMContentLoaded', () =>{
   document.getElementById('button-up').addEventListener("mouseup", (e) => mouseupUp(e, player))
   document.getElementById('button-down').addEventListener("mousedown", (e) => mousedownDown(e, player))
   document.getElementById('button-down').addEventListener("mouseup", (e) => mouseupDown(e, player))
-  document.addEventListener('keydown', (e) => {handleKeyDown(e, player = player)()})
+  document.addEventListener('keydown', (e) => {handleKeyDown(e, player)})
   document.addEventListener('keyup', (e) => {handleKeyUp(e.keyCode)()})
 
   let drawImage = () => {
@@ -87,156 +84,7 @@ document.addEventListener('DOMContentLoaded', () =>{
   requestAnimationFrame(drawImage);
   window.drawImage = drawImage
 
-  // let sprite = new Image();
-  // sprite.src = './assets/icy_tower_sprites2.png'
-  // sprite.className = "sprite"
-  //
-  // let x = 350;
-  // let y = 380;
-  //
-  // let srcX;
-  // let srcY;
-  //
-  // let sheetWidth = 656;
-  // let sheetHeight = 278;
-  //
-  // let cols = 18;
-  // let rows = 4
-  //
-  // let width = sheetWidth/ cols;
-  // let height = sheetHeight / rows;
-  //
-  // let currentFrame = 0;
-  //
-  // let mouseDownID = -1;
-  //
-  // let handleKeyDown = (n) => {
-  //   switch(n){
-  //     case 37:
-  //     return mousedownLeft;
-  //
-  //     case 38:
-  //     return mousedownUp;
-  //
-  //     case 39:
-  //     return mousedownRight;
-  //
-  //     case 40:
-  //     return mousedownDown;
-  //
-  //     default:
-  //     return () =>{};
-  //   }
-  // };
-  //
-  //
-  // const controls = {
-  //   left: false,
-  //   right: false,
-  //   up: false,
-  //   down: false
-  // }
-  //
-  // let handleKeyUp = (n) => {
-  //   switch(n){
-  //     case 37:
-  //     return mouseupLeft;
-  //
-  //     case 38:
-  //     return mouseupUp;
-  //
-  //     case 39:
-  //     return mouseupRight;
-  //
-  //     case 40:
-  //     return mouseupDown;
-  //
-  //     default:
-  //     return () => {};
-  //   }
-  // };
-  //
-  // let mousedownRight = (e) => {
-  //   if (mouseDownID ==-1) {
-  //     mouseDownID = setInterval(moveRight, 20)
-  //   }
-  // }
-  // let mouseupRight = (e) => {
-  //   if (mouseDownID != -1) {
-  //     clearInterval(mouseDownID)
-  //     mouseDownID = -1;
-  //   }
-  // }
-  //
-  // let mousedownLeft = (e) => {
-  //   if (mouseDownID ==-1) {
-  //     mouseDownID = setInterval(moveLeft, 20)
-  //   }
-  // }
-  // let mouseupLeft = (e) => {
-  //   if (mouseDownID != -1) {
-  //     clearInterval(mouseDownID)
-  //     mouseDownID = -1;
-  //   }
-  // }
-  //
-  // let mousedownUp = (e) => {
-  //   if (mouseDownID ==-1) {
-  //     mouseDownID = setInterval(moveUp, 20)
-  //   }
-  // }
-  // let mouseupUp = (e) => {
-  //   if (mouseDownID != -1) {
-  //     clearInterval(mouseDownID)
-  //     mouseDownID = -1;
-  //   }
-  // }
-  //
-  // let mousedownDown = (e) => {
-  //   if (mouseDownID ==-1) {
-  //     mouseDownID = setInterval(moveDown, 20)
-  //   }
-  // }
-  // let mouseupDown = (e) => {
-  //   if (mouseDownID != -1) {
-  //     clearInterval(mouseDownID)
-  //     mouseDownID = -1;
-  //   }
-  // }
-  //
-  // let moveLeft = () => {
-  //   ctx.clearRect(x, y, width, height)
-  //   x -= 10;
-  //   // srcY -=50;
-  // }
-  //
-  // let moveRight = () => {
-  //   ctx.clearRect(x, y, width, height)
-  //   x += 10;
-  //   // srcX +=50;
-  // }
-  //
-  // let moveUp = () => {
-  //   ctx.clearRect(x, y, width, height)
-  //   y -= 10;
-  // }
-  //
-  // let moveDown = () => {
-  //   ctx.clearRect(x, y, width, height)
-  //   y += 10;
-  // }
-  // document.getElementById('button-right').addEventListener("mousedown", mousedownRight)
-  // document.getElementById('button-right').addEventListener("mouseup", mouseupRight)
-  // document.getElementById('button-left').addEventListener("mousedown", mousedownLeft)
-  // document.getElementById('button-left').addEventListener("mouseup", mouseupLeft)
-  // document.getElementById('button-up').addEventListener("mousedown", mousedownUp)
-  // document.getElementById('button-up').addEventListener("mouseup", mouseupUp)
-  // document.getElementById('button-down').addEventListener("mousedown", mousedownDown)
-  // document.getElementById('button-down').addEventListener("mouseup", mouseupDown)
-  //
-  // document.addEventListener('keydown', (e) => {handleKeyDown(e.keyCode)()})
-  // document.addEventListener('keyup', (e) => {handleKeyUp(e.keyCode)()})
-  //
+
   // let a = 1;
   // let updateFrame = () => {
   //   // if (currentFrame === 0){
@@ -255,30 +103,5 @@ document.addEventListener('DOMContentLoaded', () =>{
   //   }
   //   srcX = currentFrame * width + 6.2;
   //   srcY = 0 * height
-  //
-  //   if (x >= 670 ){
-  //     x = 670;
-  //   } else if (x < 0){ x = 0 }
-  //   if (y <= -40 ){
-  //     y = -40;
-  //   } else if (y > 370) {
-  //     y = 370;
-  //   }
   // }
-  //
-  // let drawImage = () => {
-  //   updateFrame();
-  //   ctx.drawImage(sprite, srcX, srcY, width, height, x, y, width, height)
-  //   requestAnimationFrame(drawImage)
-  // }
-  //
-  // requestAnimationFrame(drawImage);
-  // window.drawImage = drawImage
-  // document.body.childNodes[1].appendChild(sprite)
 });
-
-// export default player;
-// MovingObject.prototype.draw = (ctx) => {
-//   ctx.fillStyle = this.sprite
-// };
-// window.MovingObject = MovingObject;
